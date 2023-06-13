9e4baf698c132833a050b10397ca06091b697e60
Ol To <olto@Ols-MacBook-Air.local>
2023-06-13 21:33:33 +0200

    build + create chanks

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {Suspense} from "react";
 import "./scss/app.scss";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
-import NotFounded from "./pages/NotFounded";
-import Cart from "./pages/Cart";
-import Product from "./pages/Product";
 import Main from "./layouts/Main";
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ "./pages/Cart"));
+const NotFounded = React.lazy(() => import(/* webpackChunkName: "NotFounded" */ "./pages/NotFounded"));
+const Product = React.lazy(() => import(/* webpackChunkName: "Product" */ "./pages/Product"));
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<Product />} />
-        <Route path="*" element={<NotFounded />} />
+        <Route path="cart" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Cart />
+          </Suspense>
+          }/>
+        <Route path="product/:id" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Product />
+          </Suspense>
+        } />
+        <Route path="*" element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <NotFounded />
+          </Suspense>
+        } />
       </Route>
     </Routes>
   );
